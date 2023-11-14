63c16019d1cfa9baea160eff5f753b0680b8d53f
Crowdbotics <team@crowdbotics.com>
2023-11-14 19:23:23 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lifehack-app-44523",
+  url: "https://lifehack-app-44523.botics.co",
+  api: "https://lifehack-app-44523.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
